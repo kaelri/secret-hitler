f8556b064badf7abd10620a3820e18965397a997
Michael Engard <kaelri@gmail.com>
2021-08-15 18:42:10 -0400

    Switch to Node.

---
+++ app.js
@@ -0,0 +1,13 @@
+var fs   = require('fs');
+var http = require('http');
+var url  = require('url'); 
+
+http.createServer(function (req, res) {
+
+    fs.readFile('app.html', function(err, data) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		return res.end();
+	});
+
+}).listen(8080);
--- wp-content/themes/secrethitler/functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-add_action('after_setup_theme', function(){
-	show_admin_bar(false);
-});
